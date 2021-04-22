0cb2fc78f646821311b73c490d275396adbc1821
ans4572 <ans4572@naver.com>
2021-04-22 16:20:33 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@
 #include "mbed.h"
 
 // Initialize a pins to perform analog input and digital output fucntions
-AnalogIn   ain(A0);
-DigitalOut dout(LED1);
+//AnalogIn   ain(A0);
+//DigitalOut dout(LED1);
 
 int main(void)
 {
@@ -26,15 +26,16 @@ int main(void)
         // test the voltage on the initialized analog pin
         //  and if greater than 0.3 * VCC set the digital pin
         //  to a logic 1 otherwise a logic 0
-        if(ain > 0.3f) {
-            dout = 1;
-        } else {
-            dout = 0;
-        }
+       // if(ain > 0.3f) {
+        //    dout = 1;
+        //} else {
+        //    dout = 0;
+        //}
         
         // print the percentage and 16 bit normalized values
-        printf("percentage: %3.3f%%\n", ain.read()*100.0f);
-        printf("normalized: 0x%04X \n", ain.read_u16());
-        wait(0.2f);
+        //printf("percentage: %3.3f%%\n", ain.read()*100.0f);
+       // printf("normalized: 0x%04X \n", ain.read_u16());'
+         printf("Hello");
+        //wait(0.2f);
     }
 }
